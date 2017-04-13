00cd1d1b1528838263bb6ab75cb64e4ad03b9001
Peter Tonoli <peter+github@metaverse.org>
2017-04-13 12:20:36 +1000

    Rename Assetrequest.php to make it directory index #2

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+if (!include('config.php'))
+   die('Error: Could not open \"config.php\", please edit and rename \"config.php.dist\"');
+?>
+<html>
+<body>
+<form action="FilesenderRestClient.php" method="post">
+<input type="hidden" name="itemid" value="<?php echo htmlspecialchars($_REQUEST['itemid']) ;?>">
+
+<?php
+if (isset($_SERVER['HTTP_REFERER'])) {
+?>
+<input type="hidden" name="referrer" value="<?php echo htmlspecialchars($_SERVER['HTTP_REFERER']) ;?>">
+<?php
+}
+?>
+You have requested items that have conditional access. By submitting your email address, you are agreeing to the following conditions of access <?php echo ACCESS_CONDITIONS ;?>
+<br />
+E-mail: <input type="text" name="email" /></p>
+<br />
+<p><input type="submit" value="Request the items"></p>
+</form>
