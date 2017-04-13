c61e14be7911776d913116d48ccc694c44568c2b
Peter Tonoli <peter+github@metaverse.org>
2017-04-13 15:34:26 +1000

    Removed debug code. Removed more hardcoded constants. Now relying on submitted item ID

--- FilesenderRestClient.php
+++ FilesenderRestClient.php
@@ -1,10 +1,42 @@
+/*
+ This file contains source code from the Filesender project.
+
+ *
+ * FileSender www.filesender.org
+ * 
+ * Copyright (c) 2009-2012, AARNet, Belnet, HEAnet, SURFnet, UNINETT
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * *    Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * *    Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * *    Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 <?php
 require_once 'FilesenderRestClient.class.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 ini_set("error_log", "/tmp/php-error.log");
-
 
 if (!include('config.php'))
    die('Error: Could not open \"config.php\", please edit and rename \"config.php.dist\"');
@@ -54,39 +86,13 @@ function Zip($source, $destination)
     return $zip->close();
 }
 
-/*
- * FileSender www.filesender.org
- * 
- * Copyright (c) 2009-2012, AARNet, Belnet, HEAnet, SURFnet, UNINETT
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * *    Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- * *    Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- * *    Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
- *     names of its contributors may be used to endorse or promote products
- *     derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+$basename = basename($_POST['itemid']);
 
-$tempfname = tempnam ( '/tmp/' , 'ESRC' );
+$ohrmname = substr($basename, 0, 4);
 
-Zip (ASSET_BASE.'/BROW00001', $tempfname);
+$tempfname = tempnam ( '/tmp/' , $ohrmname );
+
+Zip (ASSET_BASE."/".$basename, $tempfname);
 
 try {
     $c = new FilesenderRestClient(FILESENDER_URL, 'user', FILESENDER_USERID, FILESENDER_APIKEY);
@@ -104,15 +110,15 @@ try {
      * @param string $message optional message
      * @param string $expires expiry date (yyyy-mm-dd or unix timestamp)
      * @param array $options array of selected option identifiers
-     */
+     **/
     print_r($c->sendFiles(
 	FILESENDER_USERID,
 	FILESENDER_USERID,
 	array(
 $tempfname
 ),
-  array('xx@aarnet.edu.au'),
-        'API TEST subject',
+  array($_POST['email']),
+        '['.$ohrmname.'] Your selected items are ready for download.',
         "By downloading this file, you agree to the following conditions : ".ACCESS_CONDITIONS,
 	time() + 24*60*60*30,
 	array("email_download_complete", "email_report_on_closing")
@@ -128,12 +134,18 @@ unlink ($tempfname);
 <!DOCTYPE html>
 <html>
 <body>
+<br /><br />
+Your requested items have been sent. Please check your email address <i><?php echo($_POST['email'])?></i> for the download link.
+<br /><br />
 <?php
 if (isset($_POST['referrer'])) {
 ?>
-<a href="<?php echo htmlspecialchars($_POST['referrer']) ;?>>Return to the site</a>
+<a href="<?php echo htmlspecialchars($_POST['referrer']) ;?>">Return to the site</a>
 <?php
-}
+} else
+?>
+<a href="/">Return to the site</a>
+<?php
 ?>
 </body>
 </html>
